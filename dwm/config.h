2830f9c8c91feb6ae544e39fd7e8de82977f9fbe
user7210unix <user7210@proton.me>
2025-02-05 14:17:48 +0100

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,14 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 44;        /* gaps between windows */
 static const unsigned int snap      = 3;       /* snap pixel */
-static const int user_bh            = 12;        /* 2 is the default spacing around the bar's font */
+static const int user_bh            = 14;        /* 2 is the default spacing around the bar's font */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 16;       /* horizontal padding of bar */
+static const int vertpad            = 20;       /* vertical padding of bar */
+static const int sidepad            = 206;       /* horizontal padding of bar */
 static const char *fonts[] = {"JetBrainsMonoNerdFont-Bold:size=18"};
 static const char dmenufont[] = "JetBrainsMonoNerdFont-Bold:size=18";
 static char normbgcolor[]           = "#000000";

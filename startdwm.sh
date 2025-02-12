1dca1ed766fb9c197eecf50c81fba69811cbdf3f
user7210unix <user7210@proton.me>
2025-02-12 15:40:21 +0100

    Update startdwm.sh

--- startdwm.sh
+++ startdwm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Set up monitors
-xrandr --output DP-2-8 --auto --primary
-xrandr --output DP-2-1 --auto --right-of DP-2-8
+# xrandr --output DP-2-8 --auto --primary
+# xrandr --output DP-2-1 --auto --right-of DP-2-8
 
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 slstatus &
